11178b58a1f158f6a84477da900c95caef2757c5
Stefan <koop4u@gmail.com>
2017-02-20 11:25:29 +0000

    Am refacut _taxonomies sa afiseze categoriile pe stinga

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -5,3 +5,17 @@ module Spree::BaseHelper
       link_to image_tag(image_path, img_options), spree.root_path
     end
 end
+
+def sidebar_tree(root_taxon, current_taxon, max_level = 1)
+      return '' if max_level < 1 || root_taxon.children.empty?
+      content_tag :li, class:'hidden' do
+        taxons = root_taxon.children.map do |taxon|
+          css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'active' : nil
+          content_tag :li, class: css_class do
+           link_to(taxon.name, seo_url(taxon)) +
+             sidebar_tree(taxon, current_taxon, max_level - 1)
+          end
+        end
+        safe_join(taxons, "\n")
+      end
+end
