9138595c394ded70e32e0e2005aacd3989850062
Stefan <koop4u@gmail.com>
2017-03-01 19:21:06 +0000

    Added action mailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,13 @@ Rails.application.initialize!
 
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_startstls_auto => true
+}
